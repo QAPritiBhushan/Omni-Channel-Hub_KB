77ca0eb083a525be32569a9789f268157cfd2b3d
Priti Bhushan <pritibhushanf@gmail.com>
2021-08-09 17:41:10 +0530

    Added Git ignore , and added os

--- Utilities/customLogger.py
+++ Utilities/customLogger.py
@@ -1,10 +1,20 @@
 import logging
 import logging.handlers
-
+import os
 
 class LogGen:
     @staticmethod
+
     def logGen():
+        if not os.path.exists("Logs"):
+            os.makedirs("Logs")
+
+        if not os.path.exists("Reports"):
+            os.makedirs("Reports")
+
+        if not os.path.exists("Screenshots"):
+            os.makedirs("Screenshots")
+
         logging.basicConfig(filename="Logs/KhanBankAutomationLog.log", format='%(asctime)s - %(message)s',
                             datefmt='%d-%b-%y %H:%M:%S', filemode='w')
         rotate_file = logging.handlers.RotatingFileHandler("Logs/KhanBankAutomationLog.log", maxBytes=1024 * 1024 * 20,
